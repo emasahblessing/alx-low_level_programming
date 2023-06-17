f5a009535f8b0767a389d968e557f17b12b97f01
emasahblessing <blessingemasah@gmail.com>
2023-06-16 21:05:38 -0700

    gg

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int b;
+	int e;
+
+	for (b = 48; b <= 57; b++)
+	{
+		putchar(b);
+	}
+	for (e = 97; e <= 102; e++)
+	{
+		putchar(e);
+	}
+	putchar('\n');
+	return (0);
+}
