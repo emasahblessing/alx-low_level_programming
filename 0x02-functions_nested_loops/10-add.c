c02314c43b7119b1920018ab4e6a5fbe29f4593e
emasahblessing <blessingemasah@gmail.com>
2023-06-20 20:51:33 -0700

    checker

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns results
+ * @n1: first number to add
+ * @n2: second number to add
+ * Return: integer value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
